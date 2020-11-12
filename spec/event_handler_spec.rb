ad330cb86577a439667a5de70831b85caa8c9c5f
Hongli Lai <honglilai@gmail.com>
2020-11-12 21:16:41 +0100

    More tests

--- spec/event_handler_spec.rb
+++ spec/event_handler_spec.rb
@@ -59,7 +59,7 @@ describe Pulljoy::EventHandler do
       )
     end
 
-    def stub_comment_post_request
+    def stub_comment_post
       stub_request(
         :post,
         "https://api.github.com/repos/#{event.repository.full_name}/issues/#{event.pull_request.number}/comments"
@@ -67,13 +67,13 @@ describe Pulljoy::EventHandler do
     end
 
     it 'requests a review' do
-      req_stub = stub_comment_post_request.with(body: /Please review whether it's safe to start a CI run/)
+      comment_post_req = stub_comment_post.with(body: /Please review whether it's safe to start a CI run/)
       create_event_handler.process(event)
-      expect(req_stub).to have_been_requested
+      expect(comment_post_req).to have_been_requested
     end
 
     it 'transitions to the awaiting_manual_review state' do
-      stub_comment_post_request
+      stub_comment_post
       create_event_handler.process(event)
 
       state = Pulljoy::EventHandler::State.where(
@@ -112,7 +112,7 @@ describe Pulljoy::EventHandler do
       )
     end
 
-    def stub_comment_post_request
+    def stub_comment_post
       stub_request(
         :post,
         "https://api.github.com/repos/#{event.repository.full_name}/issues/#{event.pull_request.number}/comments"
@@ -120,13 +120,13 @@ describe Pulljoy::EventHandler do
     end
 
     it 'requests a review' do
-      req_stub = stub_comment_post_request.with(body: /Please review whether it's safe to start a CI run/)
+      comment_post_req = stub_comment_post.with(body: /Please review whether it's safe to start a CI run/)
       create_event_handler.process(event)
-      expect(req_stub).to have_been_requested
+      expect(comment_post_req).to have_been_requested
     end
 
     it 'transitions to the awaiting_manual_review state' do
-      stub_comment_post_request
+      stub_comment_post
       create_event_handler.process(event)
 
       state = Pulljoy::EventHandler::State.where(
@@ -225,10 +225,9 @@ describe Pulljoy::EventHandler do
       it 'cancels the CI build' do
         query_runs_req = stub_query_runs
         cancel_run_req = stub_cancel_run
-        stub_delete_branch.to_return(status: 200)
+        stub_delete_branch
 
-        handler = create_event_handler
-        handler.process(event)
+        create_event_handler.process(event)
         expect(query_runs_req).to have_been_requested
         expect(cancel_run_req).to have_been_requested
       end
@@ -238,18 +237,163 @@ describe Pulljoy::EventHandler do
         stub_cancel_run
         delete_branch_req = stub_delete_branch
 
-        handler = create_event_handler
-        handler.process(event)
+        create_event_handler.process(event)
         expect(delete_branch_req).to have_been_requested
       end
     end
   end
 
   describe 'upon pushing new code' do
-    it 'cancels the previous CI build'
-    it 'deletes the local branch'
-    it 'requests a review'
-    it 'changes the review ID'
+    let(:event) do
+      Pulljoy::PullRequestEvent.new(
+        action: Pulljoy::PullRequestEvent::ACTION_SYNCHRONIZE,
+        repository: {
+          full_name: 'test/test'
+        },
+        user: {
+          login: 'pulljoy'
+        },
+        pull_request: {
+          number: 123,
+          head: {
+            sha: 'head',
+            repo: {
+              full_name: 'fork/test',
+            }
+          },
+          base: {
+            sha: 'base',
+            repo: {
+              full_name: 'test/test'
+            }
+          }
+        }
+      )
+    end
+
+    let(:first_review_id) { 'first-review' }
+    let(:local_branch_sha) { 'local' }
+    let(:workflow_run_id) { 1337 }
+
+    def initialize_with_awaiting_manual_review_state
+      Pulljoy::EventHandler::State.create!(
+        repo: event.repository.full_name,
+        pr_num: event.pull_request.number,
+        state_name: Pulljoy::EventHandler::STATE_AWAITING_MANUAL_REVIEW,
+        review_id: first_review_id,
+      )
+    end
+
+    def initialize_with_awaiting_ci_state
+      Pulljoy::EventHandler::State.create!(
+        repo: event.repository.full_name,
+        pr_num: event.pull_request.number,
+        state_name: Pulljoy::EventHandler::STATE_AWAITING_CI,
+        commit_sha: local_branch_sha,
+      )
+    end
+
+    def stub_query_runs
+      stub_request(
+        :get,
+        "https://api.github.com/repos/#{event.repository.full_name}/actions/runs?status=queued"
+      ).to_return(
+        status: 200,
+        headers: { 'Content-Type' => 'application/json' },
+        body: JSON.generate(
+          total_count: 1,
+          workflow_runs: [
+            {
+              id: workflow_run_id,
+              head_sha: local_branch_sha
+            }
+          ]
+        )
+      )
+    end
+
+    def stub_cancel_run
+      stub_request(
+        :post,
+        "https://api.github.com/repos/#{event.repository.full_name}" \
+          "/actions/runs/#{workflow_run_id}/cancel"
+      ).to_return(status: 200)
+    end
+
+    def stub_delete_branch
+      stub_request(
+        :delete,
+        "https://api.github.com/repos/#{event.repository.full_name}" \
+          "/git/refs/heads/pulljoy/#{event.pull_request.number}"
+      ).to_return(status: 200)
+    end
+
+    def stub_comment_post
+      stub_request(
+        :post,
+        "https://api.github.com/repos/#{event.repository.full_name}/issues/#{event.pull_request.number}/comments"
+      ).to_return(status: 200, body: '{}')
+    end
+
+    it 'cancels the previous CI build' do
+      initialize_with_awaiting_ci_state
+
+      query_runs_req = stub_query_runs
+      cancel_run_req = stub_cancel_run
+      stub_delete_branch
+      stub_comment_post
+
+      create_event_handler.process(event)
+      expect(query_runs_req).to have_been_requested
+      expect(cancel_run_req).to have_been_requested
+    end
+
+    it 'deletes the local branch' do
+      initialize_with_awaiting_ci_state
+
+      stub_query_runs
+      stub_cancel_run
+      delete_branch_req = stub_delete_branch
+      stub_comment_post
+
+      create_event_handler.process(event)
+      expect(delete_branch_req).to have_been_requested
+    end
+
+    it 'requests a review' do
+      initialize_with_awaiting_manual_review_state
+      comment_post_req = stub_comment_post.with(body: /Please review whether it's safe to start a CI run/)
+
+      create_event_handler.process(event)
+
+      expect(comment_post_req).to have_been_requested
+    end
+
+    it 'transitions to the awaiting_manual_review state' do
+      initialize_with_awaiting_manual_review_state
+      stub_comment_post
+
+      create_event_handler.process(event)
+
+      state = Pulljoy::EventHandler::State.where(
+        repo: event.repository.full_name,
+        pr_num: event.pull_request.number
+      ).first
+      expect(state.state_name).to eq(Pulljoy::EventHandler::STATE_AWAITING_MANUAL_REVIEW)
+    end
+
+    it 'changes the review ID' do
+      initialize_with_awaiting_manual_review_state
+      stub_comment_post
+
+      create_event_handler.process(event)
+
+      state = Pulljoy::EventHandler::State.where(
+        repo: event.repository.full_name,
+        pr_num: event.pull_request.number
+      ).first
+      expect(state.review_id).not_to eq(first_review_id)
+    end
   end
 
   describe 'upon responding to a review request' do
@@ -278,5 +422,16 @@ describe Pulljoy::EventHandler do
     describe 'if we are in the awaiting_ci state' do
       it 'reports the result'
     end
+  end
+
+  describe 'when deleting a local branch' do
+    it 'does nothing when the branch does not exist'
+    it 'raises the API error if the error is not related to the branch not existing'
+  end
+
+  describe 'when canceling a CI run' do
+    it 'finds the run in the run in queued workflow runs'
+    it 'finds the run in the run in in-progress workflow runs'
+    it 'does nothing when there is no CI run'
   end
 end
