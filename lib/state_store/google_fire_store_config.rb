449caa4a5201ead3b00dbc11e58d7a7cf118fd69
Hongli Lai <honglilai@gmail.com>
2021-03-20 15:34:10 +0100

    Refactoring: put Google Cloud client library configuration into a reusable class

--- lib/state_store/google_fire_store_config.rb
+++ lib/state_store/google_fire_store_config.rb
@@ -1,26 +1,13 @@
 # frozen_string_literal: true
 
 require 'dry-struct'
+require_relative '../gcloud_config'
 require_relative '../utils'
 
 module Pulljoy
   module StateStore
-    class GoogleFireStoreConfig < Dry::Struct
-      transform_keys(&:to_sym)
-
+    class GoogleFireStoreConfig < GCloudConfig
       attribute :collection_name, Types::Coercible::String
-      attribute? :project_id, Types::Coercible::String.optional
-      attribute? :credentials,
-                 Types::Coercible::String |
-                 Types::Coercible::Hash |
-                 Types::Strict::Nil
-      attribute? :scope,
-                 Types::Coercible::String |
-                 Types::Coercible::Array.of(Types::Coercible::String) |
-                 Types::Strict::Nil
-      attribute? :timeout, Types::Coercible::Integer.optional
-      attribute? :endpoint, Types::Coercible::String.optional
-      attribute? :emulator_host, Types::Coercible::String.optional
     end
   end
 end
